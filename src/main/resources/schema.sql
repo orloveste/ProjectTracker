3e02ed965039af9da6431778cae32092310d9f6f
orloveste <orlovevm@yahoo.com>
2020-02-08 15:42:03 +0200

    schema.sql foreign key

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,20 +14,24 @@ create table projectTracker.project
     idProject int unsigned auto_increment primary key,
     nameProject varchar(60) not null ,
     shortNameProject varchar(12) not null ,
-    descriptionProject varchar(250) not null ,
-    statusIdProject int not null
+    descriptionProject varchar(250) not null,
+    idStatus int unsigned,
+    foreign key (idStatus) references projectTracker.status(idStatus)
 );
 create table projectTracker.item
 (
     itemId int unsigned auto_increment primary key ,
     titleItem varchar(60)not null ,
     descriptionItem varchar(250)not null ,
-    statusIdProject int not null ,
-    typeId int not null
+    idStatus int unsigned,
+    typeId int unsigned,
+    foreign key (typeId) references projectTracker.type (idType),
+    foreign key (idStatus) references projectTracker.status(idStatus)
 );
 create table projectTracker.comment
 (
     commentId int unsigned auto_increment primary key ,
     textComment varchar(3000) not null ,
-    itemId int not null
+    itemId int unsigned,
+    foreign key (itemId) references projectTracker.item (itemId)
 )
