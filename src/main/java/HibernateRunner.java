0393982a2418a032e0cc8e1b994a748617a89fbb
orloveste <orlovevm@yahoo.com>
2020-02-09 13:23:38 +0200

    test update in DB runJava

--- src/main/java/HibernateRunner.java
+++ src/main/java/HibernateRunner.java
@@ -2,15 +2,19 @@ import java.util.Objects;
 
 public class HibernateRunner {
     public static void main(String[] args) {
-        TypeService typeService = TypeService.getInstance();
-        Type typeToUpdate = typeService.getType(1);
-        typeToUpdate.nameType="bug";
-        typeService.updateType(typeToUpdate); // update by id
+        ProjectService projectService = ProjectService.getInstance();
+        Project projectToUpdate = projectService.getProject(1);
+        projectToUpdate.nameProject = "First Project update";
+        projectService.updateProject(projectToUpdate);
 
 
 
         HibernateUtil.shutdown();
     }
 }
+/*TypeService typeService = TypeService.getInstance();
+        Type typeToUpdate = typeService.getType(1);
+        typeToUpdate.nameType="bug";
+        typeService.updateType(typeToUpdate);*/ // update by id
 /* CommentService commentService = CommentService.getInstance();
         System.out.println(commentService.getComment(1));*/ // find by ud comment
