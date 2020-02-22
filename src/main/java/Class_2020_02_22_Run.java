21b9f940c7bb03101757fd3fffd28a2b573cc3a0
orloveste <orlovevm@yahoo.com>
2020-02-22 11:20:18 +0200

    Inter project - Class_2020_02_22_Run - connect menu 1 project - 1 list project - with DB

--- src/main/java/Class_2020_02_22_Run.java
+++ src/main/java/Class_2020_02_22_Run.java
@@ -1,7 +1,16 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Class_2020_02_22_Run {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
         Scanner reader = new Scanner(System.in);
         printHelloText();
         String enterProgram = reader.nextLine();
@@ -15,6 +24,7 @@ public class Class_2020_02_22_Run {
             }
             else if (userChooseOption == 1){
                 printProjectMenu();
+
                 userChooseOption = reader.nextInt();
                 if (userChooseOption < 0 || userChooseOption >2){
                     System.out.println("Option is not valid");
@@ -22,6 +32,13 @@ public class Class_2020_02_22_Run {
                 }
                 else if (userChooseOption == 1){
                     System.out.println("first option");
+                    Transaction tx = session.beginTransaction();
+                    Query query = session.createQuery("from Project");
+                    List<Project> projectList = query.list();
+                    for (Project textProject : projectList){
+                        System.out.println("List of projects::"+ textProject.idProject +", "+textProject.nameProject);
+                    } tx.rollback();
+                    sessionFactory.close();
                 }else if (userChooseOption == 2){
                     System.out.println("second option");
                 } else {
@@ -30,8 +47,10 @@ public class Class_2020_02_22_Run {
             }
             else if (userChooseOption == 2){
                 printStatusMenu();
+
             }else if (userChooseOption == 3){
                 printTypeMenu();
+
             }
         }
 
@@ -43,14 +62,14 @@ public class Class_2020_02_22_Run {
     }
     private static void printFrontMenu() {
         System.out.println("Choose the option:");
-        System.out.println("1 for Project");
-        System.out.println("2 for Status of acction");
-        System.out.println("3 for Type of acction");
+        System.out.println("1 for Project"); //done
+        System.out.println("2 for Status of acction"); //done
+        System.out.println("3 for Type of acction"); //done
     }
     private static void printProjectMenu() {
         System.out.println("Project menu - Choose the option:");
-        System.out.println("1 for List of Project");
-        System.out.println("2 for Status of Projects");
+        System.out.println("1 for List of Project"); //done
+        System.out.println("2 for Status of Projects"); //todo
         //can be submenu
 //        System.out.println("3 for Items of acction in Projects");
 //        System.out.println("4 for Type of acction in Projects");
