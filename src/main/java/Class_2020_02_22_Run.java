800ade5cc08e6f5369f7a62245a4a5a7c9bcd8eb
orloveste <orlovevm@yahoo.com>
2020-02-22 10:04:11 +0200

    Inter project - Class_2020_02_22_Run - Create first options for workflow

---
+++ src/main/java/Class_2020_02_22_Run.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Class_2020_02_22_Run {
+    public static void main(String[] args) {
+        System.out.println("Hello in Project Controller!");
+
+        System.out.println("Choose the option:");
+        System.out.println("1 for Project");
+        System.out.println("2 for Status of acction");
+        System.out.println("3 for Type of acction");
+        Scanner reader = new Scanner(System.in);
+        int userChooseOption = reader.nextInt();
+
+
+    }
+}
