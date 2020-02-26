bef4cb34fea277f01933351fefaaa98e2b3f1f98
orloveste <orlovevm@yahoo.com>
2020-02-26 20:14:51 +0200

    create account

---
+++ src/main/java/Rusi_26_02_2020/Account.java
@@ -0,0 +1,4 @@
+package Rusi_26_02_2020;
+
+public class Account {
+}
---
+++ src/main/java/Rusi_26_02_2020/Main1.java
@@ -0,0 +1,9 @@
+package Rusi_26_02_2020;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Account account = new Account();
+    }
+}
+//videostreaming fara sql?
+//user - account
